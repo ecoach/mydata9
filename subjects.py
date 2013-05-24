628f01a4db64bed08e89881cd1e7ad073c2be75b
jaredtritz <jtritz@umich.edu>
2013-05-24 18:12:35 -0400

    couple mods Dennis and Mike showed me

--- subjects.py
+++ subjects.py
@@ -1,10 +1,11 @@
 from djangotailoring.subjects import DjangoSubjectLoader
-from mydata4.models import Source1
+from .models import Source1
 
 class ECoachSubjectLoader(DjangoSubjectLoader):
     sources = {
         #'MTS_SOURCE_NAME': MODEL_OBJECT_CLASS ... the table name should matche the class name
         'Source1': Source1,
+        #'Common1': Common1,
     }
 
-
+    # 
