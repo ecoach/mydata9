d39fb9e82c386326ce376940ba20bc73790abb04
jaredtritz <jtritz@umich.edu>
2013-08-10 02:39:24 -0400

    redid_quite_a_bit_with_urls_to_satisfy_reverse_lookup

--- urls.py
+++ urls.py
@@ -2,19 +2,15 @@ from django.conf.urls.defaults import patterns, include, url
 from .views import *
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import redirect_to
-
 #from mycoach import urls
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+mts_patterns = patterns('',
     # admin
     url(r'^admin/', include(admin.site.urls)),
-
-    # login / logout
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 
     # staff apps
     url(r'^staff/',         include('mypublisher.urls', namespace='mystaff')),
@@ -23,13 +19,18 @@ urlpatterns = patterns('',
     url(r'^export/',        include('myexporter.urls', namespace='myexporter')),
     url(r'^nts/',           include('nts.urls', namespace='nts')),
     #url(r'^emailer/',       include('myemailer.urls', namespace='myemailer')),
-    #url(r'^copycat/',       login_required(mycopycat.urls)),
-    #url(r'^curator/',       login_required(mycurator.urls)),
+    #url(r'^copycat/',       include('mycopycat.urls', namespace='mycopycat')),
+    #url(r'^curator/',       include('mycurator.urls', namespace='mycurator')),
     #url(r'^usage/',         include('myusage.urls', namespace='myusage')),
     url(r'^logger/',        include('mylogger.urls', namespace='mylogger')),
 
     # message project
-    #url(r'^',               include('mycoach.urls', namespace='mycoach')),
     url(r'^',               include('mycoach.urls', namespace='mycoach')),
+)
+
+urlpatterns = patterns('',
+    url(r'^coach4/',          include(mts_patterns)),
+    url(r'^coaches/',       include('myselector.urls', namespace='myselector')),
+    url(r'^',               redirect_to, {'url': '/coaches/'})
 )
 
