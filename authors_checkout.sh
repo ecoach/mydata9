57c5837f11b1a630d609a4f27f35f9f95582896e
jaredtritz <jaredtritz@gmail.com>
2013-08-11 12:46:08 -0400

    don't freeze dictionary for this project

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -2,10 +2,10 @@
 
 #export SCRIPT_DIR=$(pwd)
 
-echo 'begin script'
+echo 'begin script' 
 cd ~jtritz/bitbucket/ecoach_webapps/mydata4/mts4
 svn update
-svn update -r1416 mts.dictionary
+#svn update -r1416 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
 python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata4.settings
 echo 'end of script'
