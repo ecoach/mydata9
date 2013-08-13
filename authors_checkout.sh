6956b6da1162c5293b51440b70052f3503d38f69
jaredtritz <jtritz@umich.edu>
2013-08-13 14:17:54 -0400

    update_aarons_survey_and_remove_dictioanry_checkout_constraints

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -5,7 +5,7 @@
 echo 'begin script' 
 cd ~jtritz/bitbucket/ecoach_webapps/mydata9/mts9
 svn update
-svn update -r1445 mts.dictionary
+#svn update -r1445 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
 python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata9.settings
 echo 'end of script'
