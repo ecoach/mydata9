d43e9f6da679f8a7524c6bc8ed34d5af9154e4c0
jaredtritz <jtritz@umich.edu>
2013-08-15 20:13:14 -0400

    fix_the_missing_source_and_configure_source_for_common

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.models import User
 from .models import Source1
+from .models import Common1
 
 def configure_source_data(username):
     # THIS IS A HACK TO ENSURE ALL THE ENTRY POINTS ADD THE RIGHT SOURCE ROW FOR A USER
-
+    ret = False
     # ensure the user entry
     me = User.objects.filter(username=username)
     if len(me) < 1:
         me = User(username=username)
         me.save() 
-        #return False
     else:
         me = me[0]
 
@@ -19,10 +19,19 @@ def configure_source_data(username):
         # After the enrollment period add a copy of Albert Einstein's data to non-enrolled users 
         ss = Source1(user_id=me)
         ss.save()
-        return True
-    else:
-        # user already has source data - len(ss) should always be == 1
-        return False
-         
+        ret = True
+    
+    ss = Common1.objects.filter(user_id=me)
+    if len(ss) < 1:
+        # After the enrollment period add a copy of Albert Einstein's data to non-enrolled users 
+        ss = Common1(user_id=me)
+        ss.save()
+        ret = True
+
+    return ret
+        
+ 
+        
+ 
             
  
